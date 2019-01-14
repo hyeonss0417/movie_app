d3e82fab9e6bae08e25b0b0cf7d2be00b57daad4
hyeonsung <skyoun97@gmail.com>
2019-01-14 22:16:04 +0900

    Updates

---
+++ precache-manifest.46a1385ef9a25e1990c4289ffe4b44fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "180de1bb1f238fc129ca",
+    "url": "/movie_app/static/js/main.180de1bb.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "180de1bb1f238fc129ca",
+    "url": "/movie_app/static/css/main.96c3d330.chunk.css"
+  },
+  {
+    "revision": "852f913fa84784883a0c6b8408a659ef",
+    "url": "/movie_app/index.html"
+  }
+];
